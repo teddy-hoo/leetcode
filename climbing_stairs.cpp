437c40fb3b2089e5983efab0ff42d19259f50577
teddy-hoo <hulingchuan@hotmail.com>
2014-03-18 17:02:25 +0800

    Create climbing_stairs.cpp

---
+++ climbing_stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 0 || n == 1 || n == 2)
+            return n;
+        int *steps = new int[n];
+        steps[0] = 1;
+        steps[1] = 2;
+        for(int i = 2; i < n; i++)
+            steps[i] = steps[i - 1] + steps[i - 2];
+        return steps[n - 1];
+    }
+};
