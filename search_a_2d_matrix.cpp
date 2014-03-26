5a4aaf9a364e6599134d14594915d74017a4da13
teddy-hoo <hulingchuan@hotmail.com>
2014-03-26 19:30:21 +0800

    Create search_a_2d_matrix.cpp

---
+++ search_a_2d_matrix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int> > &matrix, int target) {
+        for(int i = 0; i < matrix.size(); i++){
+            if(matrix[i][0] > target)
+                break;
+            for(int j = 0; j < matrix[i].size(); j++){
+                if(matrix[i][j] == target)
+                    return true;
+                if(matrix[i][j] > target)
+                    break;
+            }
+        }
+        return false;
+    }
+};
