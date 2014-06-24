e26944791e1c208a805dda2d36521809f2347206
teddy-hoo <hulingchuan@hotmail.com>
2014-06-24 19:01:13 +0800

    link list circle 2

---
+++ link_list_circle_2.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  ListNode *detectCycle(ListNode *head) {
+    ListNode * first = head;
+    ListNode * second = head;
+
+    while(first != NULL && second != NULL)
+      {
+        first = first->next;
+        second = second->next;
+        if(second != NULL)
+          second = second->next;
+        if(first == second)
+          break;
+      }
+
+    if(second == NULL) return NULL;
+
+    first = head;
+    while(first!=second)
+      {
+        first = first->next;
+        second = second->next;
+      }
+    return second;
+  }
+};
