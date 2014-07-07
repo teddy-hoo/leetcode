ceb4980595ed9096e3bb6adc424dc3b04ea3aaf8
teddy-hoo <hulingchuan@hotmail.com>
2014-07-07 17:44:04 +0800

    unsolved problem added

---
+++ minimum_depth_of_binary_tree.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  int minDepth(TreeNode *root) {
+    queue<TreeNode*> q;
+    TreeNode* pot;
+    if(!root){
+      return 0;
+    }
+    TreeNode* flag = new TreeNode(-9999);
+    q.push(root);
+    
+  }
+};
