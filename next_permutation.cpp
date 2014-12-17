66c620857ef4ddf15409e3c1b3017f872902142a
Teddy Hoo <hulingchuan@hotmail.com>
2014-12-17 19:45:36 +0800

    copied code from others

---
+++ next_permutation.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void nextPermutation(vector<int> &num) {
+        if(num.size() <= 1){
+            return;
+        }
+        int end = num.size() - 1;
+        int povit = end;
+        while(povit > 0){
+            if(num[povit] > num[povit - 1]) break;
+            povit --;
+        }
+        if(povit > 0){
+            povit --; 
+            int large = end;
+            while(num[large] <= num[povit]) large --;
+            swap(num[large] , num[povit]);
+            reverse(num.begin() + povit + 1 , num.end());
+        }else{
+            reverse(num.begin() , num.end());
+        }
+    }
+};
