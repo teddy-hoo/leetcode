90c8c3cb388e70795dc57133d0d480937a6e8a12
teddy-hoo <hulingchuan@hotmail.com>
2014-12-25 23:16:36 +0800

    if there are duplicate values in the array, this algorithm will fail

--- 3_sum.cpp
+++ 3_sum.cpp
@@ -1,42 +1,35 @@
 class Solution {
 private:
-    int add(vector<int> list){
-        int sum = 0;
-        for(int i = 0; i < list.size(); i++){
-            sum += list[i];
+    map<int, int> buildMap(vector<int> num){
+        map<int, int> m;
+        for(int i = 0; i < num.size(); i++){
+            m[num[i]] = i;
         }
-        return sum;
+        return m;
     }
-    vector<vector<int> > calSum(vector<vector<int> > subsets, int target){
-        vector<vector<int> > results;
-        for(int i = 0; i < subsets.size(); i++){
-            if(subsets[i].size() == 3 && add(subsets[i]) == target){
-                results.push_back(subsets[i]);
-            }
-        }
-        return results;
-    }
-    vector<vector<int> > genSubset(vector<int> num){
+    vector<vector<int> > calSum(vector<int> num, map<int, int> m){
         vector<vector<int> > subsets;
         vector<int> subset;
-        subsets.push_back(subset);
+        map<int, int>::iterator iter;
+        
         for(int i = 0; i < num.size(); i++){
-            for(int j = 0; j < subsets; j++){
-                vector<int> tmp = subsets[j];
-                if(tmp.size() >= 3){
-                    continue;
+            for(int j = i + 1; j < num.size(); j++){
+                iter = m.find(0 - num[i] - num[j]);
+                if(iter != m.end() && iter->second > j){
+                    subset.clear();
+                    subset.push_back(num[i]);
+                    subset.push_back(num[j]);
+                    subset.push_back(iter->first);
+                    subsets.push_back(subset);
                 }
-                subsets.push_back(tmp.push_back[num[i]]);
             }
         }
         return subsets;
     }
 public:
-    vector<vector<int> > fourSum(vector<int> &num, int target) {
-        if(num.size() <= 0){
-            return results;
-        }
-        vector<vector<int> > subsets = genSubset(num);
-        return calSum(subsets, target);
+    vector<vector<int> > threeSum(vector<int> &num) {
+        sort(num.begin(), num.end());
+        map<int, int> m = buildMap(num);
+        return calSum(num, m);
     }
 };
