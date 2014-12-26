9da48a0900e052857afd0c2f92708a88e3d857b8
teddy-hoo <hulingchuan@hotmail.com>
2014-12-26 22:56:34 +0800

    a more consice solution

---
+++ majority_element.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int majorityElement(vector<int> &num) {
+        int majority = num[0];
+        int count = 0;
+        int len = num.size();
+
+        for(int i = 0; i < len; i++){
+            if(count == 0){
+                majority = num[i];
+                count++;
+            }
+            else{
+                majority == num[i] ? count++ : count--;
+                if(count >= len/2 + 1){
+                    return majority;
+                }
+            }
+        }
+        return majority;
+    }
+};
