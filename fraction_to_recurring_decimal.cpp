51b5d0402eaeef2ae6241012d8fa3d6b8d43e1d4
teddy-hoo <hulingchuan@hotmail.com>
2014-12-26 22:16:00 +0800

    got the best solution

--- fraction_to_recurring_decimal.cpp
+++ fraction_to_recurring_decimal.cpp
@@ -14,29 +14,31 @@ private:
 	string remainderToString(long long int remainder, int denominator){
 		string str = "";
 		int preValue = remainder;
+		map<long long int, int> rem;
+		map<long long int, int>::iterator iter;
+		int pos = 0;
 		while(remainder != 0){
+			iter = rem.find(remainder);
+			if(iter != rem.end()){
+				str.insert(str.begin() + rem[remainder], '(');
+				str += ")";
+				return str;
+			}
+			rem[remainder] = pos++;
 			remainder = remainder * 10;
 			int quotient = remainder / denominator;
 			str += quotient + '0';
-			if(quotient == preValue){
-				break;
-			}
-			preValue = quotient;
 			remainder = remainder % denominator;
-		}
-		if(str.size() >= 2 && str[str.size() - 1] == str[str.size() - 2]){
-			str[str.size() - 2] = '(';
-			str += ')';
 		}
 		return str;
 	}
-	string quotientToString(int quotient){
+	string quotientToString(long long int quotient){
 		string str = "";
 		if(quotient == 0){
 			return str + "0";
 		}
 		while(quotient != 0){
-			int remainder = quotient % 10;
+			long long int remainder = quotient % 10;
 			str += remainder + '0';
 			quotient = quotient / 10;
 		}
@@ -45,24 +47,26 @@ private:
 	}
 public:
 	string fractionToDecimal(int numerator, int denominator) {
+		string result;
 		if(denominator == 0){
-			return NULL;
+			return result;
 		}
-		int quotient = numerator / denominator;
-		long long int remainder = numerator % denominator;
-		string result = "";
 		if(numerator == 0){
 			return "0";
 		}
+		long long int quotient = numerator / denominator;
+		long long int remainder = numerator % denominator;
 		if(numerator < 0 ^ denominator < 0 ) {
 			result += '-';
 		}
 		string quetientStr = quotientToString(quotient);
-		string remainderStr = remainderToString(remainder, denominator);
-		if(remainderStr != ""){
-			quetientStr += ".";
-			quetientStr += remainderStr;
+		result += quetientStr;
+		if(remainder == 0){
+			return result;
 		}
+		string remainderStr = remainderToString(remainder, denominator);
+		quetientStr += ".";
+		quetientStr += remainderStr;
 		return quetientStr;
 	}
 };
