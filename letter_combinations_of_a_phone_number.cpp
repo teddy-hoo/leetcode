e0a5f10037801815c0f95971166ad9575757ae3d
teddy-hoo <hulingchuan@hotmail.com>
2014-12-27 19:08:23 +0800

    unsolved

---
+++ letter_combinations_of_a_phone_number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        int len = digits.size();
+        vector<string> results;
+        if(len <= 0){
+            return results;
+        }
+        string chars[8] = {"abc", "def", "ghi", "jkl", "mno"};
+        for(int i = 0; i < len; i++){
+            
+        }
+    }
+};
