8470d654361c3e3c7ed70a7a2e11b5dd54729692
teddy-hoo <hulingchuan@hotmail.com>
2014-12-27 22:23:36 +0800

    solved

--- letter_combinations_of_a_phone_number.cpp
+++ letter_combinations_of_a_phone_number.cpp
@@ -3,12 +3,22 @@ public:
     vector<string> letterCombinations(string digits) {
         int len = digits.size();
         vector<string> results;
+        results.push_back("");
         if(len <= 0){
             return results;
         }
-        string chars[8] = {"abc", "def", "ghi", "jkl", "mno"};
+        string characters[8] = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         for(int i = 0; i < len; i++){
-            
+            vector<string> tmp;
+            int index = digits[i] - '2';
+            string curStr = characters[index];
+            for(int j = 0; j < curStr.size(); j++){
+                for(int k = 0; k < results.size(); k++){
+                    tmp.push_back(results[k] + curStr[j]);
+                }
+            }
+            results = tmp;
         }
+        return results;
     }
 };
