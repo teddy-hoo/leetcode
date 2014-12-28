35b3bdd12461ccfaa8ed4ae9ab00adb26336adc8
teddy-hoo <hulingchuan@hotmail.com>
2014-12-28 10:04:57 +0800

    solved

--- first_missing_positive.cpp
+++ first_missing_positive.cpp
@@ -7,22 +7,25 @@ private:
     }
     void adjustArray(int A[], int len){
         for(int i = 0; i < len; i++){
-            while(i != A[i] || A[i] < len){
-                swapValue(A[i], A[A[i]]);
+            while(A[i] > 0 && i + 1 != A[i] && A[i] < len && A[i] != A[A[i] - 1]){
+                swapValue(A[i], A[A[i] - 1]);
             }
         }
     }
     int findMissing(int A[], int len){
         for(int i = 0; i < len; i++){
-            if(i + 1 >= len || A[i + 1] != i + 1){
-                return i + 1;
-            }
+        	if(A[i] != i + 1){
+        		return i + 1;
+        	}
+        	if(i + 1 >= len){
+        		return len + 1;
+        	}
         }
     }
 public:
     int firstMissingPositive(int A[], int n) {
-        if(n <- 0){
-            return 0;
+        if(n <= 0){
+            return 1;
         }
         adjustArray(A, n);
         return findMissing(A, n);
