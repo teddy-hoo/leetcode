903fa8b35856544d32ac0e19646f22fa609b2f3f
teddy-hoo <hulingchuan@hotmail.com>
2015-01-02 20:43:43 +0800

    a dp solution, but memory limit exceeded

--- string_with_concatenation_of_all_words.cpp
+++ string_with_concatenation_of_all_words.cpp
@@ -1,83 +1,82 @@
 class Solution {
 private:
-	map<char, string> genDict(vector<string> l){
-		int len = l.size();
-		map<char, string> dict;
-		for(int i = 0; i < len; i++){
-			dict[l[i][0]] = l[i];
-		}
-		return dict;
-	}
-	map<char, int> genSymbol(vector<string> l){
-		int len = l.size();
-		map<char, int> symbol;
-		for(int i = 0; i < len; i++){
-			symbol[l[i][0]] = -1;
-		}
-		return symbol;
-	}
-	bool isDistinct(string word, string s, int index){
-		int len = word.size();
-		int i = 0;
-		while(i < len){
-			if(word[i] != s[index + i]){
-				return false;
+	void genWords(vector<string> l, map<string, vecgto){
+		map<string, vector<int> > words;
+		map<string, vector<int> >::iterator iter;
+		int num = l.size();
+		for(int i = 0; i < num; i++){
+			iter = words.find(l[i]);
+			if(iter == words.end()){
+				vector<int> indices;
+				indices.push_back(i);
+				words[l[i]] = indices;
 			}
-			i++;
-		}
-		return true;
-	}
-	void clearSymbol(map<char, int> &symbol, int pos){
-		map<char, int>::iterator iter;
-		for(iter = symbol.begin(); iter != symbol.end(); iter++){
-			if(pos == - 1 || iter->second <= pos){
-				iter->second = -1;
+			else{
+				iter->second.push_back(i);
 			}
 		}
+		return words;
 	}
-	bool checkFull(map<char, int> symbol){
-		map<char, int>::iterator iter;
-		for(iter = symbol.begin(); iter != symbol.end(); iter++){
-			if(iter->second < 0){
-				return false;
+	vector<vector<int> > genPos(string s, map<string, vector<int> > words, int step){
+		int len = s.size() - step;
+		map<string, vector<int> >::iterator iter;
+		vector<vector<int> > wordPos;
+		vector<int> emptyVector;
+		for(int i = 0; i <= len; i++){
+			string word = s.substr(i, step);
+			iter = words.find(word);
+			if(iter != words.end()){
+				wordPos.push_back(iter->second);
+			}
+			else{
+				wordPos.push_back(emptyVector);
 			}
 		}
-		return true;
+		return wordPos;
 	}
 public:
     vector<int> findSubstring(string S, vector<string> &L) {
-        vector<int> result;
-        int lens = S.size();
-        int lenl = L.size();
-        if(lens <= 0 || lenl <= 0 || L[0].size() <= 0){
-        	return result;
-        }
-        map<char, string> dict = genDict(L);
-        map<char, string>::iterator iterD;
-        map<char, int> symbol = genSymbol(L);
-        for(int i = 0; i < lens; i++){
-        	int cur = i;
-        	iterD = dict.find(S[cur]);
-        	while(iterD != dict.end()){
-        		if(!isDistinct(iterD->second, S, cur)){
-        			break;
-        		}
-        		if(symbol[S[cur]] != -1){
-        			i = symbol[S[cur]] + iterD->second.size();
-        			clearSymbol(symbol, symbol[S[cur]]);
-        		}
-        		symbol[S[cur]] = cur;
-        		if(checkFull(symbol)){
-        			result.push_back(i);
-        		}
-        		cur = cur + iterD->second.size();
-        		if(cur >= lens){
-        			break;
-        		}
-        		iterD = dict.find(S[cur]);
-        	}
-        	clearSymbol(symbol, -1);
-        }
-        return result;
-    }
+		map<string,vector<int> > words;
+		vector<vector<int> > wordPos;
+		vector<int> result;
+
+		int wordNum = L.size();
+		int wordLen = L[0].size();
+
+		words = genWords(L);
+		wordPos = genPos(S, words, wordLen);
+		words.clear();
+
+		bool symbol[500];
+		memset(symbol, false, sizeof(bool) * 500);
+		int maxLen = wordLen * wordNum;
+		int maxIndex = S.size() - maxLen;
+		for(int i = 0; i <= maxIndex; i++)
+		{
+			int j = 0;
+			while(j < maxLen)
+			{
+				vector<int> pos = wordPos[i + j];
+				if(pos.size() <= 0){
+					break;
+				}
+				int k = 0;
+				for(; k < pos.size(); k++){
+					if(!symbol[pos[k]]){
+						symbol[pos[k]] = true;
+						break;
+					}
+				}
+				if(k == pos.size()){
+					break;
+				}
+				j += wordLen;
+			}
+			if(j == maxLen){
+				result.push_back(i);
+			}
+			memset(symbol, false, sizeof(bool) * 500);
+		}
+		return result;
+	}
 };
