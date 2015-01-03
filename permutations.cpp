c3e883a7f06d98e4608e286ab5139bc4ff4feecf
teddy-hoo <hulingchuan@hotmail.com>
2015-01-03 16:31:33 +0800

    more clear code

--- permutations.cpp
+++ permutations.cpp
@@ -1,35 +1,36 @@
-class Solution {  
-private:  
-    vector<vector<int> >result;   
-    int data[10];  
-    int sign[10];  
+class Solution {
+private:
+    void searchPermute(vector<int> num, vector<int> &indices, vector<vector<int> > &results, vector<bool> &used){
+        int len = num.size();
+        if(indices.size() == len){
+            vector<int> result;
+            for(int i = 0; i < len; i++){
+                result.push_back(num[indices[i]]);
+            }
+            results.push_back(result);
+        }
+        else{
+            for(int i = 0; i < len; i++){
+                if(!used[i]){
+                    used[i] = true;
+                    indices.push_back(i);
+                    searchPermute(num, indices, results, used);
+                    used[i] = false;
+                    indices.pop_back();
+                }
+            }
+        }
+    }
 public:  
     vector<vector<int> > permute(vector<int> &num) {  
-        result.clear();  
-        memset(sign,0,sizeof(sign));  
-        int n=num.size();  
-        dfs(n,0,num);  
-        return result;  
-    }  
-    void dfs(int n,int index,vector<int> &num)  
-    {  
-        if(index==n)  
-        {  
-            vector<int >t;  
-            for(int i=0;i<n;++i)
-                t.push_back(num[data[i]]);  
-            result.push_back(t);  
-            return;  
-        }  
-        for(int i=0;i<n;++i)  
-        {  
-            if(sign[i]==0)  
-            {  
-                sign[i]=1;  
-                data[index]=i;  
-                dfs(n,index+1,num);  
-                sign[i]=0;  
-            }  
-        }  
-    }  
+        vector<vector<int> > results;
+        int len = num.size();
+        if(len <= 0){
+            return results;
+        }
+        vector<bool> used(len, false);
+        vector<int> indices;
+        searchPermute(num, indices, results, used);
+        return results;  
+    }
 };
