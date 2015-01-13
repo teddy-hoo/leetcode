3a833f9b0c7f8d68940d7f2169ce6e6ec8f8a3bc
teddy-hoo <hulingchuan@hotmail.com>
2015-01-13 20:12:47 +0800

    build a graph, then bfs, but time limit exceeded, O(n^2) time. travel the whole graph.

---
+++ word_ladder.cpp
@@ -0,0 +1,70 @@
+class Solution {
+private:
+    bool converted(string s1, string s2){
+        int identical = 0;
+        for(int i = 0; i < s1.size(); i++){
+            if(s1[i] == s2[i]){
+                identical++;
+            }
+        }
+        return identical == 2;
+    }
+    map<string, vector<string> > buildGraph(unordered_set<string> dict, map<string, bool> &visited){
+        unordered_set<string>::iterator i;
+        unordered_set<string>::iterator j;
+        map<string, vector<string> > graph;
+        
+        for(i = dict.begin(); i != dict.end(); i++){
+            vector<string> neighbors;
+            for(j = dict.begin(); j != dict.end(); j++){
+                if(*i != *j && converted(*i, *j)){
+                    neighbors.push_back(*j);
+                }
+            }
+            graph[*i] = neighbors;
+            visited[*i] = false;
+        }
+        
+        return graph;
+    }
+public:
+    int ladderLength(string start, string end, unordered_set<string> &dict) {
+        
+        dict.insert(start);
+        dict.insert(end);
+        
+        map<string, vector<string> > graph;
+        map<string, bool> visited;
+        graph = buildGraph(dict, visited);
+        
+        queue<string> q;
+        q.push(start);
+        visited[start] = true;
+        int step = 1;
+        int cur = 1;
+        int next = 0;
+        while(!q.empty()){
+            string str = q.front();
+            q.pop();
+            if(str == end){
+                return step;
+            }
+            vector<string> neighbors = graph[str];
+            for(int i = 0; i < neighbors.size(); i++){
+                string s = neighbors[i];
+                if(!visited[s]){
+                    q.push(s);
+                    visited[s] = true;
+                    next++;
+                }
+            }
+            cur--;
+            if(cur == 0){
+                step++;
+                cur = next;
+                next = 0;
+            }
+        }
+        return -1;
+    }
+};
