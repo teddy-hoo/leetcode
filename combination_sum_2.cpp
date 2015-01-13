c866929075b70702af114a67ede7779a2cf6ef00
teddy-hoo <hulingchuan@hotmail.com>
2015-01-13 10:15:24 +0800

    more efficient, more lclean

--- combination_sum_2.cpp
+++ combination_sum_2.cpp
@@ -2,44 +2,23 @@ class Solution {
 private:
     vector<int> numbers;
     int t;
-    bool duplicate(vector<vector<int> > result, vector<int> mr){
-        int i, j, k;
-        for(i = 0; i < result.size(); i++){
-            for(j = 0, k = 0; j < result[i].size(); j++, k++){
-                if(k == mr.size() || result[i][j] != mr[k]){
-                    break;
-                }
-            }
-            if(k == mr.size() && j == result[i].size()){
-                return true;
-            }
-        }
-        return false;
-    }
     void searchCombination(int begin, vector<vector<int> > &results, vector<int> &middleResult, int sum){
         int len = numbers.size();
-        if(begin == len){
-            if(sum == t){
-                if(!duplicate(results, middleResult)){
-                    results.push_back(middleResult);
-                }
-                return;
-            }
-            return;
-        }
         if(sum == t){
-            if(!duplicate(results, middleResult)){
-                results.push_back(middleResult);
-            }
+            results.push_back(middleResult);
             return;
         }
         for(int i = begin; i < len; i++){
-            int s = sum + numbers[i];
-            if(s <= t){
-                middleResult.push_back(numbers[i]);
-                searchCombination(i + 1, results, middleResult, s);
-                middleResult.pop_back();
+            if(i > begin && numbers[i] == numbers[i - 1]){
+                continue;
             }
+            int s = sum + numbers[i];
+            if(s > t){
+                continue;
+            }
+            middleResult.push_back(numbers[i]);
+            searchCombination(i + 1, results, middleResult, s);
+            middleResult.pop_back();
         }
     }
 public:
