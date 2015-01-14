338af83cc4b002b1497583467070d097a1794833
teddy-hoo <hulingchuan@hotmail.com>
2015-01-14 15:43:24 +0800

    more efficient

--- best_time_to_buy_and_sell_stock.cpp
+++ best_time_to_buy_and_sell_stock.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     int maxProfit(vector<int> &prices) {
-        if(prices.size() <= 1)
+        
+        int len = prices.size();
+        
+        if(len <= 0){
             return 0;
+        }
+
         int max = 0;
         int min = prices[0];
-        int i = 1;
-        int j = 0;
-        for(;i < prices.size(); i++){
-            if(prices[i] < prices[i - 1]){
-                if(prices[i] < min)
-                    min = prices[i];
-                continue;
+        for(int i = 1; i < len; i++){
+            if(prices[i] < min){
+                min = prices[i];
             }
-            if(prices[i] - min > max)
+            else if(prices[i] - min > max){
                 max = prices[i] - min;
+            }
         }
+
         return max;
     }
 };
