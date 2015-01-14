3ee17ba0847e615a5d5b6eeacc7b4b294c3764ec
teddy-hoo <hulingchuan@hotmail.com>
2015-01-14 09:58:39 +0800

    memory limit exceeded.

--- word_ladder_2.cpp
+++ word_ladder_2.cpp
@@ -1,96 +1,90 @@
 class Solution {
 private:
-    map<string, vector<string> > graph;
-    vector<vector<string> > results;
-    string des;
-    bool converted(string s1, string s2){
-        int identical = 0;
-        int len = s1.size();
-        for(int i = 0; i < len; i++){
-            if(s1[i] == s2[i]){
-                identical++;
-            }
-        }
-        return identical == len - 1;
-    }
     map<string, vector<string> > buildGraph(unordered_set<string> dict,
-        map<string, bool> &visited){
-        unordered_set<string>::iterator i;
-        unordered_set<string>::iterator j;
+        string start, string end){
+
         map<string, vector<string> > graph;
-        
-        for(i = dict.begin(); i != dict.end(); i++){
+        map<string, bool> visited;
+
+        queue<string> q;
+        q.push(start);
+        bool reachEnd = false;
+        int cur = 1;
+        int next = 0;
+
+        while(!q.empty() && cur != 0){
+            
+            string str = q.front();
+            q.pop();
+
             vector<string> neighbors;
-            for(j = dict.begin(); j != dict.end(); j++){
-                if(*i != *j && converted(*i, *j)){
-                    neighbors.push_back(*j);
+            for(int i = 0; i < str.size(); i++){
+                string tmp = str;
+                for(char j = 'a'; j <= 'z'; j++){
+                    tmp[i] = j;
+                    if(str == tmp){
+                        continue;
+                    }
+                    if(tmp == end){
+                        reachEnd = true;
+                        neighbors.push_back(tmp);
+                    }
+                    else if(!reachEnd &&  dict.count(tmp) > 0 && visited.count(tmp) == 0){
+                        next++;
+                        visited[tmp] = true;
+                        neighbors.push_back(tmp);
+                        q.push(tmp);
+                    }
                 }
             }
-            graph[*i] = neighbors;
-            visited[*i] = false;
+
+            cur--;
+            if(cur == 0 && !reachEnd){
+                cur = next;
+                next = 0;
+            }
+
+            graph[str] = neighbors;
+        }
+
+        if(!reachEnd){
+            graph.clear();
         }
         
         return graph;
     }
-    void dfs(vector<string> path, int &minlen, map<string, bool> visited){
+    void dfs(map<string, vector<string> > graph, vector<string> path,
+        string end, vector<vector<string> > &results){
         
         string start = path.back();
 
-        if(start == des){
+        if(start == end){
             results.push_back(path);
-            minlen = path.size() < minlen ? path.size() : minlen;
-            return;
-        }
-
-        if(path.size() + 1 > minlen){
             return;
         }
         
         vector<string> neighbors = graph[start];
         for(int i = 0; i < neighbors.size(); i++){
-            string neighbor = neighbors[i];
-            if(!visited[neighbor]){
-                visited[neighbor] = true;
-                path.push_back(neighbor);
-                dfs(path, minlen, visited);
-                visited[neighbor] = false;
-                path.pop_back();
-            }
+            path.push_back(neighbors[i]);
+            dfs(graph, path, end, results);
+            path.pop_back();
         }
+        
     }
 public:
     vector<vector<string> > findLadders(string start, string end, 
       unordered_set<string> &dict) {
 
-        dict.insert(start);
-        dict.insert(end);
-        
-        map<string, bool> visited;
-        graph = buildGraph(dict, visited);
+        vector<vector<string> > results;
+        map<string, vector<string> > graph;
+
+        graph = buildGraph(dict, start, end);
 
         vector<string> path;
         path.push_back(start);
 
-        int minlen = dict.size() + 1;
-        visited[start] = true;
-        des = end;
+        dfs(graph, path, end, results);
 
-        dfs(path, minlen, visited);
-
-        int min = dict.size() + 1;
-        for(int i = 0; i < results.size(); i++){
-            if(results[i].size() < min){
-                min = results[i].size();
-            }
-        }
-
-        vector<vector<string> > newResults;
-        for(int i = 0; i < results.size(); i++){
-            if(results[i].size() == min){
-                newResults.push_back(results[i]);
-            }
-        }
-
-        return newResults;
+        return results;
     }
 };
