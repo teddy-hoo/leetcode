4eded43ea21f5b54a48d430f80f56f39c06ede36
teddy-hoo <hulingchuan@hotmail.com>
2015-01-17 17:40:42 +0800

    using binary search algorithm, more efficient

--- search_in_rotated_sorted_array.cpp
+++ search_in_rotated_sorted_array.cpp
@@ -1,14 +1,35 @@
 class Solution {
 public:
     int search(int A[], int n, int target) {
-        bool flag = false;
-        int i = 0;;
-        int count = 0;
-        if(A[0] > target)
-            i = n - 1;
-        for(i = 0; A[i] != target && count < n; count++){
-            i = flag? i - 1: i + 1;
+        if(n <= 0){
+            return -1;
         }
-        return count == n? -1 : i;
+
+        int begin = 0;
+        int end = n - 1;
+        while(begin <= end){
+            int middle = (begin + end) / 2;
+            if(A[middle] == target){
+                return middle;
+            }
+            if(begin == middle || A[begin] < A[middle]){
+                if(A[middle] < target || A[begin] > target){
+                    begin = middle + 1;
+                }
+                else{
+                    end = middle - 1;
+                }
+            }
+            else{
+                if(target > A[end] || target < A[middle]){
+                    end = middle - 1;
+                }
+                else{
+                    begin = middle + 1;
+                }
+            }
+        }
+
+        return -1;
     }
 };
