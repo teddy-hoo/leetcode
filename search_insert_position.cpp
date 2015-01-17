d07104abb803f7dd71ed131fc3298aa9b84d16c9
teddy-hoo <hulingchuan@hotmail.com>
2015-01-17 16:19:53 +0800

    change to binary search.

--- search_insert_position.cpp
+++ search_insert_position.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     int searchInsert(int A[], int n, int target) {
-        int indexa;
-        int isfind = -1;
-        for(indexa = 0; indexa < n; indexa ++){
-            if(A[indexa] >= target){
-                isfind = indexa;
-                break;
+        
+        if(n <= 0){
+            return 0;
+        }
+
+        int begin = 0;
+        int end = n - 1;
+        while(begin <= end){
+            int middle = (begin + end) / 2;
+            if(A[middle] == target){
+                return middle;
+            }
+            else if(A[middle] < target){
+                begin = middle + 1;
+            }
+            else{
+                end = middle - 1;
             }
         }
-        if(isfind == -1)
-            return n;
-        return isfind;
+        return begin;
     }
 };
