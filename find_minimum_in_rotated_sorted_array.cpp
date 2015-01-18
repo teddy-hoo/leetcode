e7dccc23434609d5570bce7fc53fce6017d13186
teddy-hoo <hulingchuan@hotmail.com>
2015-01-18 12:05:38 +0800

    binary search, try to define the minimum element in a proper way.

--- find_minimum_in_rotated_sorted_array.cpp
+++ find_minimum_in_rotated_sorted_array.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-  int findMin(vector<int> &num) {
-    int length = num.size();
+    int findMin(vector<int> &num) {
+        
+        int len = num.size();
 
-    if(length <= 0){
-      return -1;
-    }
+        if(len <= 0){
+            return -1;
+        }
 
-    int begin = 0;
-    int end   = length - 1;
+        int begin = 0;
+        int end = len - 1;
+        while(begin < end){
+            int middle = (begin + end) >> 1;
+            if(num[begin] <= num[middle]){
+                if(num[end] < num[begin]){
+                    begin = middle + 1;
+                }
+                else{
+                    return num[begin];
+                }
+            }
+            else{
+                if(num[middle - 1] < num[middle]){
+                    end = middle - 1;
+                }
+                else{
+                    return num[middle];
+                }
+            }
+        }
 
-    while(1){
-
-      if(num[begin] <= num[end]){
         return num[begin];
-      }
-
-      int middle = (begin + end) / 2 + 1;
-
-      if(middle - 1 >= 0 && num[middle] < num[middle - 1]){
-        return num[middle];
-      }
-
-      if(num[middle] >= num[begin]){
-        begin = middle + 1;
-      }
-      else{
-        end = middle - 1;
-      }
-
     }
-
-  }
 };
