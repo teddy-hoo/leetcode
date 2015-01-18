f1b082e8b6fa1995c189c87532593f32919fdcfa
teddy-hoo <hulingchuan@hotmail.com>
2015-01-18 11:32:30 +0800

    binary search, more efficient.

--- search_a_2d_matrix.cpp
+++ search_a_2d_matrix.cpp
@@ -1,16 +1,53 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int> > &matrix, int target) {
-        for(int i = 0; i < matrix.size(); i++){
-            if(matrix[i][0] > target)
+        int row = matrix.size();
+        if(row <= 0){
+            return false;
+        }
+
+        int col = matrix[0].size();
+        if(col <= 0){
+            return false;
+        }
+
+        int beginr = 0;
+        int endr = row - 1;
+        int targetr = -1;
+        while(beginr <= endr){
+            int middle = (beginr + endr) >> 1;
+            if(target < matrix[middle][0]){
+                endr = middle - 1;
+            }
+            else if(target > matrix[middle][col - 1]){
+                beginr = middle + 1;
+            }
+            else{
+                targetr = middle;
                 break;
-            for(int j = 0; j < matrix[i].size(); j++){
-                if(matrix[i][j] == target)
-                    return true;
-                if(matrix[i][j] > target)
-                    break;
             }
         }
+
+        if(targetr < 0){
+            return false;
+        }
+
+        int beginc = 0;
+        int endc = col - 1;
+        while(beginc <= endc){
+            int middle = (beginc + endc) >> 1;
+            if(target == matrix[targetr][middle]){
+                return true;
+            }
+            else if(target > matrix[targetr][middle]){
+                beginc = middle + 1;
+            }
+            else{
+                endc = middle - 1;
+            }
+        }
+
         return false;
+
     }
 };
