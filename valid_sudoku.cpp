d6bc8e38adba84e1d55e238013b76595c0127310
teddy-hoo <hulingchuan@hotmail.com>
2015-01-18 17:08:54 +0800

    more clean code

--- valid_sudoku.cpp
+++ valid_sudoku.cpp
@@ -1,20 +1,27 @@
-class Solution{
+class Solution {
 public:
-	bool isValidSudoku(vector<vector<char> > &board) {
-        // Note: The Solution object is instantiated only once.
-		vector<vector<bool> > rows(9, vector<bool>(9,false));
-		vector<vector<bool> > cols(9, vector<bool>(9,false));
-		vector<vector<bool> > blocks(9, vector<bool>(9,false));
+    bool isValidSudoku(vector<vector<char> > &board) {
+        
+        vector<vector<bool> > row(9, vector<bool> (9, false));
+       	vector<vector<bool> > col(9, vector<bool> (9, false));
+       	vector<vector<bool> > blo(9, vector<bool> (9, false));
 
-		for(int i = 0; i < 9; i++)
-			for(int j = 0; j < 9; j++)
-			{
-				if(board[i][j] == '.')continue;
-				int num = board[i][j] - '1';
-				if(rows[i][num] || cols[j][num] || blocks[i - i%3 + j/3][num])
-					return false;
-				rows[i][num] = cols[j][num] = blocks[i - i%3 + j/3][num] = true;
-			}
-		return true;
+       	for(int i = 0; i < 9; i++){
+       		for(int j = 0; j < 9; j++){
+       		    if(board[i][j] == '.'){
+       		        continue;
+       		    }
+       			int bi = i / 3 * 3 + j / 3;
+       			int val = board[i][j] - '1';
+       			if(row[i][val] || col[j][val] || blo[bi][val]){
+       				return false;
+       			}
+       			row[i][val] = true;
+       			col[j][val] = true;
+       			blo[bi][val] = true;
+       		}
+       	}
+
+       	return true;
     }
 };
