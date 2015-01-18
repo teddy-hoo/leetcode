023cc959988caa0adc05cf3f8720efabdeb378e0
teddy-hoo <hulingchuan@hotmail.com>
2015-01-19 07:46:58 +0800

    more clean code.

--- longest_substring_without_repeating_characters.cpp
+++ longest_substring_without_repeating_characters.cpp
@@ -4,25 +4,19 @@ public:
         int len = 0;
         int max = 0;
         int start = 0;
-		int locs[256];  
+        int locs[256];  
         memset(locs, -1, sizeof(int) * 256);
 
         for(int i = 0; i < s.size(); i++){
-        	if(locs[s[i]] < 0){
-        		locs[s[i]] = i;
-        		len++;
-        	}
-        	else{
-        		if(locs[s[i]] < start){
-        			len++;
-        		}
-        		else{
-        			max = max > len ? max : len;
-        			len = i - locs[s[i]];
-        			start = locs[s[i]] + 1;
-        		}
-        		locs[s[i]] = i;
-        	}
+            if(locs[s[i]] < 0 || locs[s[i]] < start){
+                len++;
+            }
+            else{
+                max = max > len ? max : len;
+                len = i - locs[s[i]];
+                start = locs[s[i]] + 1;
+            }
+            locs[s[i]] = i;
         }
 
         max = max > len ? max : len;
