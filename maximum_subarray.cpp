e707ffe6639e272ccf39bd3ca0ae9b8c3a6a7365
teddy-hoo <hulingchuan@hotmail.com>
2015-01-22 17:29:51 +0800

    more clean code.

--- maximum_subarray.cpp
+++ maximum_subarray.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int maxSubArray(int A[], int n) {
-        if(!A)
-            return 0;
-        int subsum = A[0];
-        int maxsum = A[0];
-        vector<int> sum;
-        for(int i = 1; i < n; i++){
-            if(subsum <= 0){
-                if(subsum > maxsum)
-                    maxsum = subsum;
-                subsum = A[i];
-            }
-            else
-                subsum += A[i];
-            if(subsum > maxsum)
-                maxsum = subsum;
+        if(n <= 0){
+            return -1;
         }
-        return maxsum;
+        
+        int max = A[0];
+        int cur = 0;
+        for(int i = 0; i < n; i++){
+            if(cur + A[i] < 0){
+                max = A[i] > max ? A[i] : max;
+                cur = 0;
+            }
+            else{
+                cur += A[i];
+                max = cur > max ? cur : max;
+            }
+        }
+        
+        return max;
     }
 };
