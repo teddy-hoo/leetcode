42bd11669b3c68b5d463ca582efc592b81709845
teddy-hoo <hulingchuan@hotmail.com>
2015-01-25 12:59:23 +0800

    O(0) space, O(n) time.

--- add_two_numbers.cpp
+++ add_two_numbers.cpp
@@ -9,54 +9,45 @@
 class Solution {
 public:
     ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+        
         int carry = 0;
         int sum;
-        ListNode *result = new ListNode(0);
-        ListNode *p, *p1 = l1, *p2 = l2;
-        p = result;
-        while(p1 && p2){
+        ListNode *pre = new ListNode(0);
+        ListNode *p1 = l1;
+        ListNode *p2 = l2;
+        pre->next = p1;
+        
+        while(p1 != NULL && p2 != NULL){
             sum = p1->val + p2->val + carry;
-            if(sum >= 10){
-                carry = 1;
-                p->next = new ListNode(sum % 10);
-            }
-            else{
-                carry = 0;
-                p->next = new ListNode(sum % 10);
-            }
-            p = p->next;
+            sum > 9? (sum -= 10, carry = 1): carry = 0;
+            p1->val = sum;
+            pre = p1;
             p1 = p1->next;
             p2 = p2->next;
         }
-        while(p1){
+        while(p1 != NULL && carry != 0){
             sum = p1->val + carry;
-            if(sum >= 10){
-                carry = 1;
-                p->next = new ListNode(sum % 10);
-            }
-            else{
-                carry = 0;
-                p->next = new ListNode(sum % 10);
-            }
-            p = p->next;
+            sum > 9? (sum -= 10, carry = 1): carry = 0;
+            p1->val = sum;
+            pre = p1;
             p1 = p1->next;
         }
-        while(p2){
+        p1 = pre;
+        if(carry == 0 && p2 != NULL){
+            p1->next = p2;
+            return l1;
+        }
+        while(p2 != NULL && carry != 0){
             sum = p2->val + carry;
-            if(sum >= 10){
-                carry = 1;
-                p->next = new ListNode(sum % 10);
-            }
-            else{
-                carry = 0;
-                p->next = new ListNode(sum % 10);
-            }
-            p = p->next;
+            sum > 9? (sum -= 10, carry = 1): carry = 0;
+            p2->val = sum;
+            p1->next = p2;
+            p1 = p1->next;
             p2 = p2->next;
         }
         if(carry){
-            p->next = new ListNode(carry);
+            p1->next = new ListNode(carry);
         }
-        return result->next;
+        return l1;
     }
 };
