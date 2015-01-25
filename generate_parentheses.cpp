9fc0bb97a73da05fc05002713727d357f63c235b
teddy-hoo <hulingchuan@hotmail.com>
2015-01-25 14:37:42 +0800

    optimize the code.

--- generate_parentheses.cpp
+++ generate_parentheses.cpp
@@ -1,13 +1,24 @@
+/**
+ * backtracking algorithm
+ * time:   O(2^n)
+ * space:  O(2^n)
+ * date:   25-01-2015
+ * author: Teddy
+ */
+
 class Solution {
 private:
   void recursion(string result, int left, int right, vector<string> &results){
-    if(left == 0 && right == 0){
+    if(right == 0){
       results.push_back(result);
       return;
     }
+    
     if(left > 0){
       recursion(result + '(', left - 1, right, results);
     }
+    
+    // guarantee the sequence is right when left < right
     if(left < right){
       recursion(result + ')', left, right - 1, results);
     }
@@ -15,10 +26,13 @@ private:
 public:
   vector<string> generateParenthesis(int n){
     vector<string> results;
+    
     if(n <= 0){
       return results;
     }
+
     recursion("", n, n, results);
+
     return results;
   }
 };
