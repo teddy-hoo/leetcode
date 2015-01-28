e9ab06d047f3c3e7d37688ae63bd54aa0d2a8aae
teddy-hoo <hulingchuan@hotmail.com>
2015-01-28 21:43:39 +0800

    more clean code, more fast.

--- jump_game.cpp
+++ jump_game.cpp
@@ -1,26 +1,29 @@
+/**
+ * Greedt algorithm
+ * Time: O(n)
+ * Space: O(1)
+ * Author: Teddy
+ * Date: 28-1-2015
+ */
+
 class Solution {
 public:
     bool canJump(int A[], int n) {
-        if(n == 1)
+        
+        if(n <= 0){
             return true;
-        int cur_len = A[0];
-        int max_len = cur_len, max_pos = cur_len;
-        for(int cur = 1; cur < n;){
-            if(cur == cur_len + 1){
-                if(A[max_pos] == 0)
-                    return false;
-                cur_len = max_pos + A[max_pos];
-                cur = max_pos + 1;
-                max_len = cur_len;
-                max_pos = cur_len;
-                continue;
-            }
-            if(A[cur] + cur > max_len){
-                max_len = A[cur] + cur;
-                max_pos = cur;
-            }
-            cur++;
         }
-        return true;
+        
+        int curStep = 1;
+        int i;
+        for(i = 0; i < n; i++){
+            if(curStep <= 0){
+                break;
+            }
+            curStep--;
+            curStep = curStep > A[i] ? curStep : A[i];
+        }
+        
+        return i == n;
     }
 };
