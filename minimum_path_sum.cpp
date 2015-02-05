2a65549a859bf5fedcae7e167d7dc78135eb0b13
teddy-hoo <hulingchuan@hotmail.com>
2015-02-05 08:00:53 +0800

    more clean code

--- minimum_path_sum.cpp
+++ minimum_path_sum.cpp
@@ -1,33 +1,43 @@
-class Solution {
-private:
-  int min(int x, int y){
-    return x >= y ? y : x;
-  }
-public:
-  int minPathSum(vector<vector<int> > &grid) {
-    int         row    = grid.size();
-    int         column = grid[0].size();
-    vector<vector<int> > result;
-    vector<int> path;
+/**
+ * Dynamic programming solution
+ * Time:   O(mn)
+ * Space:  O(mn)
+ * Author: Teddy
+ * Date:   05-02-2015
+ */
 
-    path.push_back(grid[0][0]);
-    for(int i = 1; i < column; ++i){
-      path.push_back(grid[0][i] + path[i - 1]);
-    }
-    result.push_back(path);
-    path.clear();
-    for(int i = 1; i < row; ++i){
-      for(int j = 0; j < column; ++j){
-        if(j - 1 >= 0){
-          path.push_back(grid[i][j] + min(result[i - 1][j], path[j - 1]));
+class Solution {
+public:
+    int minPathSum(vector<vector<int> > &grid) {
+        
+        int row = grid.size();
+        int col = grid[0].size();
+        
+        if(row <= 0 || col <= 0){
+            return 0;
         }
-        else{
-          path.push_back(grid[i][j] + result[i - 1][j]);
+        
+        vector<vector<int> > sums(row, vector<int> (col, 0));
+        
+        sums[0][0] = grid[0][0];
+        
+        // init first row
+        for(int i = 1; i < col; i++){
+            sums[0][i] = grid[0][i] + sums[0][i - 1];
         }
-      }
-      result.push_back(path);
-      path.clear();
+        
+        // init first col
+        for(int i = 1; i < row; i++){
+            sums[i][0] = grid[i][0] + sums[i - 1][0];
+        }
+        
+        for(int i = 1; i < row; i++){
+            for(int j = 1; j < col; j++){
+                int min = sums[i - 1][j] < sums[i][j - 1] ? sums[i - 1][j] : sums[i][j - 1];
+                sums[i][j] = grid[i][j] + min;
+            }
+        }
+        
+        return sums[row - 1][col - 1];
     }
-    return result[row - 1][column - 1];
-  }
 };
